d4d5aa36fb1a32c8d7cc8b1f8ffe4e38e31df80f
Guilherme Teixo <guilhermesantosjp@gmail.com>
2023-06-23 18:18:21 -0300

    lógica base do programa

--- adivinhação/program.c
+++ adivinhação/program.c
@@ -1,7 +1,28 @@
 #include <stdio.h>
 
 int main() {
-	printf("Bem-vindo ao jogo de adivinhação!\n");
 
+int numerosecreto = 9;
+int chute;
+int acertou = (chute == numerosecreto);
+int errou  = (chute != numerosecreto);
+
+	printf("Bem-vindo ao jogo de adivinhação!\n");
+	printf("Insira o seu chute abaixo: \n");
+	scanf("%d", &chute);
+	printf("O seu chute foi %d ", chute);
+
+	if(acertou){
+		printf("parabéns, está correto!\n");
+	}
+	else{
+
+		if(chute>numerosecreto){
+			printf("e o seu palpite foi maior que o  número secreto!\n");
+		}
+		if(chute<numerosecreto){
+			printf("e  o seu palpite foi menor que o  número secreto!\n");
+		} 
+	}
 
 }
